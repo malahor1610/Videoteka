985ad81632d4b6cdb03c035c9107b77a8115fea8
Malahor <malahor1610@gmail.com>
2024-09-17 21:59:26 +0200

    disable few unused modules

--- src/main/java/com/github/malahor/videoteka/VideotekaApplication.java
+++ src/main/java/com/github/malahor/videoteka/VideotekaApplication.java
@@ -2,8 +2,24 @@ package com.github.malahor.videoteka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
+import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.client.RestClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+      AopAutoConfiguration.class,
+      MultipartAutoConfiguration.class,
+      GsonAutoConfiguration.class,
+      RestTemplateAutoConfiguration.class,
+      RestClientAutoConfiguration.class,
+      ErrorMvcAutoConfiguration.class,
+      WebSocketServletAutoConfiguration.class,
+    })
 public class VideotekaApplication {
 
   public static void main(String[] args) {
