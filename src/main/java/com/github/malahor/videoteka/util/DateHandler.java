4085aab01adc8c7e448c5775e8c52f4cd5a1b679
Malahor <malahor1610@gmail.com>
2024-09-26 20:25:54 +0200

    display dates always in polish style

--- src/main/java/com/github/malahor/videoteka/util/DateHandler.java
+++ src/main/java/com/github/malahor/videoteka/util/DateHandler.java
@@ -4,18 +4,25 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.format.FormatStyle;
+import java.util.Locale;
 
 public class DateHandler {
+
+  private static final String LANGUAGE = "pl";
 
   public static LocalDate localDateOf(String releaseDate) {
     try {
       return LocalDate.parse(releaseDate, DateTimeFormatter.ISO_DATE);
     } catch (DateTimeParseException e) {
-      return LocalDate.parse(releaseDate, DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
+      return LocalDate.parse(releaseDate, localFormat());
     }
   }
 
   public static String accurateDate(String releaseDate) {
-    return localDateOf(releaseDate).format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
+    return localDateOf(releaseDate).format(localFormat());
+  }
+
+  private static DateTimeFormatter localFormat() {
+    return DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).localizedBy(Locale.of(LANGUAGE));
   }
 }
